471b07a969d795a3f9035f9a3a4fd9b7265c823b
Rebecca Hannah Quinn <rebeccahannahquinn@gmail.com>
2023-02-02 19:53:38 +0000

    final program practice upload

---
+++ week02/nameAndAge.py
@@ -0,0 +1,11 @@
+yourName = input("What is your name? ")
+yourAge = input("How old are you? ")
+
+print(f"Hello \t{yourName}, your age is {yourAge}.")
+
+addIt = (21 -4)
+print(addIt)
+
+doesEqual = (2==3)
+print(doesEqual)
+
