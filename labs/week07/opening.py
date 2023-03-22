e3624870e1cbb1a464caa681a85d0541087fb6bf
Rebecca Hannah Quinn <rebeccahannahquinn@gmail.com>
2023-03-22 13:33:51 +0000

    opening and reading and writing files

--- labs/week07/opening.py
+++ labs/week07/opening.py
@@ -1,5 +1,14 @@
 FILENAME = "data.txt"
 
-with open(FILENAME, 'r') as f:
+with open(FILENAME, 'rt') as f:
+    for data in f:
+        print(data.strip()) #, end="") another strip option, strip best for linux etc
+
+with open("data2.txt", "w+") as f:
+    f.write("how now brown cow\n")
+    f.write("how now brown cow\n")
+    f.seek(0) #starts read from byte 0 or the beginning no need to 'cat' file to see text
     data = f.read()
     print(data)
+
+print("done")
