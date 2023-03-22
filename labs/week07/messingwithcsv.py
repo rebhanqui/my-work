96703d2c72b27eac42a8b178f4ac080902005f9b
Rebecca Hannah Quinn <rebeccahannahquinn@gmail.com>
2023-03-22 14:08:46 +0000

    error rm

--- labs/week07/messingwithcsv.py
+++ labs/week07/messingwithcsv.py
@@ -3,5 +3,4 @@ import csv
 FILENAME = "data.csv"
 with open(FILENAME, 'rt') as file:
     csvReader = csv.reader(file, delimiter = ',') #delimiter can be anything
-    for line in csvReader:
-    
+    #for line in csvReader:
